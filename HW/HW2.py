abfb00c25bc2797e8d04f579500993bbcb0ba416
陳辰鈺 <160983679+ChadChenYu@users.noreply.github.com>
2025-09-16 11:34:26 +0800

    Create HW2.py

---
+++ HW/HW2.py
@@ -0,0 +1,30 @@
+##請用四種方法算 f(n) = 2**n (2的 n 次方）
+## way 1:
+def power2n(n):
+    return 2 ** n
+  ## 複雜度O(1)
+
+## way 2:
+def power2n(n):
+    if n == 0:
+        return 1
+    return power2n(n-1) + power2n(n-1)
+  ## 複雜度O(2^n)
+
+## way 3:
+memo = {0: 1}
+
+def power2n(n):
+    if n in memo:
+        return memo[n]
+    memo[n] = power2n(n-1) + power2n(n-1)
+    return memo[n]
+  ## 複雜度O(n)
+
+## way 4:
+def power2n(n):
+    if n == 0:
+        return 1
+    return 2 * power2n(n-1)
+  ## 複雜度O(n)
+
